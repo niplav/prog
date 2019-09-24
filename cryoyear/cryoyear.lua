54f376d3e19ec66add39f79f2ceca06ee445d0fd
niplav <niplav@posteo.net>
2019-09-24 13:38:26 +0200

    Added visualizations.

--- cryoyear/cryoyear.lua
+++ cryoyear/cryoyear.lua
@@ -1,19 +1,19 @@
 --curent age of the user of this script
-curage=20
+curage=21
 
 actval={78.36, 78.64, 78.66, 78.67, 78.68, 78.69, 78.69, 78.70, 78.71, 78.71, 78.72, 78.72, 78.73, 78.73, 78.74, 78.75, 78.75, 78.77, 78.79, 78.81, 78.83, 78.86, 78.88, 78.91, 78.93, 78.96, 78.98, 79.01, 79.03, 79.06, 79.09, 79.12, 79.15, 79.18, 79.21, 79.25, 79.29, 79.32, 79.37, 79.41, 79.45, 79.50, 79.55, 79.61, 79.66, 79.73, 79.80, 79.87, 79.95, 80.03, 80.13, 80.23, 80.34, 80.46, 80.59, 80.73, 80.88, 81.05, 81.22, 81.42, 81.62, 81.83, 82.05, 82.29, 82.54, 82.80, 83.07, 83.35, 83.64, 83.94, 84.25, 84.57, 84.89, 85.23, 85.58, 85.93, 86.30, 86.68, 87.08, 87.49, 87.92, 88.38, 88.86, 89.38, 89.91, 90.47, 91.07, 91.69, 92.34, 93.01, 93.70, 94.42, 95.16, 95.94, 96.72, 97.55, 98.40, 99.27, 100.14, 101.02, 101.91}
 
 --value of one year of life
 val_year=50000
 --probability of cryonics success
-prob_succ=0.1
+prob_succ=0.05
 --years gained by cryonics
-years_gain=80
+years_gain=10000
 --probability of being preserved
-prob_pres=0.6
+prob_pres=0.5
 
 --starting value for probability for signing up to cryo, exponentially decreasing
-decay=0.95
+decay=0.995
 
 --probability of still signing up for cryonics at a given age
 
@@ -86,7 +86,7 @@ end
 --I'll assume that insurance companies make these so that they make a guaranteed profit
 
 function life_ins(age)
-	return 50000
+	return 110000
 end
 
 function cost(age)
@@ -98,5 +98,5 @@ function value(age)
 end
 
 for age=curage,math.floor(actval[curage]) do
-	print(age .. ": " .. value(age))
+	print(value(age) .. ": " .. age)
 end
