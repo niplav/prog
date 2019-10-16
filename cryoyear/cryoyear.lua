cd333097518d265b0bb27c3db8f035b5a5787514
niplav <niplav@posteo.net>
2019-10-17 01:20:24 +0200

    A little daygame model.

--- cryoyear/cryoyear.lua
+++ cryoyear/cryoyear.lua
@@ -1,19 +1,19 @@
 --curent age of the user of this script
-curage=21
+curage=22
 
 actval={78.36, 78.64, 78.66, 78.67, 78.68, 78.69, 78.69, 78.70, 78.71, 78.71, 78.72, 78.72, 78.73, 78.73, 78.74, 78.75, 78.75, 78.77, 78.79, 78.81, 78.83, 78.86, 78.88, 78.91, 78.93, 78.96, 78.98, 79.01, 79.03, 79.06, 79.09, 79.12, 79.15, 79.18, 79.21, 79.25, 79.29, 79.32, 79.37, 79.41, 79.45, 79.50, 79.55, 79.61, 79.66, 79.73, 79.80, 79.87, 79.95, 80.03, 80.13, 80.23, 80.34, 80.46, 80.59, 80.73, 80.88, 81.05, 81.22, 81.42, 81.62, 81.83, 82.05, 82.29, 82.54, 82.80, 83.07, 83.35, 83.64, 83.94, 84.25, 84.57, 84.89, 85.23, 85.58, 85.93, 86.30, 86.68, 87.08, 87.49, 87.92, 88.38, 88.86, 89.38, 89.91, 90.47, 91.07, 91.69, 92.34, 93.01, 93.70, 94.42, 95.16, 95.94, 96.72, 97.55, 98.40, 99.27, 100.14, 101.02, 101.91}
 
 --value of one year of life
 val_year=50000
 --probability of cryonics success
-prob_succ=0.05
+prob_succ=0.1
 --years gained by cryonics
-years_gain=10000
+years_gain=100
 --probability of being preserved
-prob_pres=0.5
+prob_pres=0.6
 
 --starting value for probability for signing up to cryo, exponentially decreasing
-decay=0.995
+decay=0.9
 
 --probability of still signing up for cryonics at a given age
 
@@ -86,7 +86,7 @@ end
 --I'll assume that insurance companies make these so that they make a guaranteed profit
 
 function life_ins(age)
-	return 110000
+	return 90000
 end
 
 function cost(age)
---
+++ daygame/daygame.lua
@@ -0,0 +1,61 @@
+--Is daygame worth it? A little model designed to find that out.
+
+--TODO: Sources for nearly all of these numbers
+
+--A bit under minimum wage
+hour_val=5
+--Price of middle range quality prostitution+a sense of pride and accomplishment
+prost_cost=500
+pride_val=250
+first_lay_val=prost_cost+pride_val
+--How much a date costs (rough estimate)
+date_cost=20
+--The average number of dates before a lay
+avg_dates=2
+
+--One every 15 minutes seems fair, but most experienced daygamers seem to do less?
+appr_per_hour=4
+--The definite upper limit seems to be 1 in 30 for professionals
+--but for the average person not willing to travel for daygame,
+--that seems a bit much
+ratio_expert=0.02
+--The general consensus seems to be 1 in 100 for beginners, but
+--I've read reports of guys not getting any lays within the first ~200
+--approaches.
+ratio_beginner=0.005
+
+function ratio(appr)
+	--grows to approximate the expert ratio, starting from the beginner ratio
+	return ratio_beginner+(ratio_expert-ratio_beginner)*(1-500/(appr+500))
+end
+
+function benefit(appr)
+	local lay_ratio=ratio(appr)
+	--positive side-effects of daygame of behavior
+	--it seems like those grow very slowly after an initial surge
+	local flowthrough_val=75*math.log(appr)
+	--print("lays val: " .. first_lay_val*math.sqrt(lay_ratio*appr))
+	--print("flowthrough val: " .. flowthrough_val)
+	return first_lay_val*math.sqrt(lay_ratio*appr)+flowthrough_val
+end
+
+function cost(appr)
+	--I could be working minimum wage!
+	local opportunity_cost=hour_val*appr/appr_per_hour
+	--mental cost is at first positive (the first approaches are very hard)
+	--then daygame becomes an enjoyable activity and the mental cost is negative
+	--i.e a benefit, but the enjoyment fades over time
+	--TODO: too many magic numbers
+	local mental_cost=(0.999^appr)*0.3*-(math.log(appr*2+10)-7)
+	--one can abuse the ratio to find out how many dates one will have to pay
+	--maybe a constant factor is not appropriate?
+	local date_ratio=3*ratio(appr)
+	--print("opportunity cost: " .. opportunity_cost)
+	--print("mental cost: " .. appr*mental_cost)
+	--print("date cost: " .. date_ratio*date_cost*avg_dates)
+	return opportunity_cost+appr*mental_cost+date_ratio*date_cost*avg_dates
+end
+
+for i=1,10000 do
+	print((benefit(i)-cost(i)) .. ": " .. i)
+end
